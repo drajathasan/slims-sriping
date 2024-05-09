aa6204bf9ed5b5c4696acea393ddbf0e09a5ed08
Drajat Hasan <drajathasan20@gmail.com>
2024-05-09 18:27:43 +0700

    Update searchbox

--- template-src/components/Searchbox.vue
+++ template-src/components/Searchbox.vue
@@ -1,17 +1,40 @@
+<script setup>
+    const state = reactive({
+        dropDown: false,
+        advanceSearch: false
+    })
+
+    const showDropDown = (evt) => {
+        state.dropDown = true
+    }
+
+    onMounted(() => {
+        document.querySelector('body')?.addEventListener('click', function(el) {
+            if (el.target.getAttribute('id') !== 'showDropDown') {
+                state.dropDown = false
+            }
+        })
+    })
+</script>
 <template>
     <section class="text-white text-center py-16 md:py-22">
         <h1 class="text-5xl font-bold">Find Something Awesome</h1>
         <p class="text-xl py-3">We have many collection to you, lets type some keyword and we will serve you</p>
-        <div class="flex flex-row bg-white mx-64 py-3">
-            <div class="text-gray-800">
-                <span class="px-4">Any field</span>
-                <ul class="absolute bg-white text-left min-w-48 max-w-fit p-8 mt-5">
-                    <li>1</li>
-                    <li>2</li>
-                    <li>3</li>
-                    <li>4</li>
+        <div class="flex justify-start bg-white mx-64">
+            <div class="flex flex-row text-gray-800 w-full">
+                <button @click="showDropDown($event)" id="showDropDown" class="text-left cursor-pointer w-2/12 h-12 px-4 py-3">Any field</button>
+                <ul v-if="state.dropDown" class="absolute bg-white text-left min-w-48 max-w-fit p-5 mt-14">
+                    <li class="pt-1 cursor-pointer" v-for="li in ['Author','Subject','ISBN/ISSN']">{{ li }}</li>
                 </ul>
+                <input type="text" class="outline-none h-12 py-3 px-2 curor-pointer w-8/12" placeholder="keyword(s)"/>
+                <div class="flex w-2/12 h-12 justify-end">
+                    <button class="px-3 my-1 mr-4 bg-blue-600 text-white">Search</button>
+                </div>
             </div>
+        </div>
+        <button class="py-5">Advance Search</button>
+        <div v-if="advanceSearch" class="flex justify-start bg-white mx-64">
+            <h1>Hai</h1>
         </div>
     </section>
 </template>
